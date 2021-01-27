e9c4ea44c6b414a0cda91889751b441f0b510160
Shivender Chaudhary <shivendermeel@gmail.com>
2021-01-27 22:55:50 +0530

    Second Commit
    Completed The project provided by freeCodeCamp.org. All test cases are passing on freeCodeCamp.org

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,24 +1,147 @@
-class SudokuSolver {
+const N = 9;
 
-  validate(puzzleString) {
+class SudokuSolver {
+  validate(puzzleString) {}
+
+  letterToNumber(row){
+    if(row.toUpperCase()==="A")return 1;
+    else if(row.toUpperCase()==="B")return 2;
+    else if(row.toUpperCase()==="C")return 3;
+    else if(row.toUpperCase()==="D")return 4;
+    else if(row.toUpperCase()==="E")return 5;
+    else if(row.toUpperCase()==="F")return 6;
+    else if(row.toUpperCase()==="G")return 7;
+    else if(row.toUpperCase()==="H")return 8;
+    else if(row.toUpperCase()==="I")return 9;
+    else return "none";
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
-
+    let grid=this.transform(puzzleString);
+    row=this.letterToNumber(row);
+    if(grid[row-1][column-1]!==0) return false;
+    for(let i=0;i<N;i++) if(grid[row-1][i]==value) return false;
+    return true;
   }
 
   checkColPlacement(puzzleString, row, column, value) {
-
+    let grid=this.transform(puzzleString);
+    row=this.letterToNumber(row);
+    if(grid[row-1][column-1]!==0) return false;
+    for(let i=0;i<N;i++) if(grid[i][column-1]==value) return false;
+    return true;
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
+    let grid=this.transform(puzzleString);
+    row=this.letterToNumber(row);
+    if(grid[row-1][column-1]!==0) return false;
+    let startRow=row-(row%3),startCol=column-(column%3);
+    for(let i=0;i<3;i++)
+      for(let j=0;j<3;j++)
+        if(grid[i+startRow][j+startCol]==value) return false;
+    return true;
+  }
 
+  isSafe(board, row, col, num) {
+    for (let d = 0; d < board.length; d++) {
+      if (board[row][d] == num) {
+        return false;
+      }
+    }
+
+    for (let r = 0; r < board.length; r++) {
+      if (board[r][col] == num) {
+        return false;
+      }
+    }
+
+    let sqrt = Math.sqrt(board.length);
+    let boxRowStart = row - (row % sqrt);
+    let boxColStart = col - (col % sqrt);
+
+    for (let r = boxRowStart; r < boxRowStart + sqrt; r++) {
+      for (let d = boxColStart; d < boxColStart + sqrt; d++) {
+        if (board[r][d] == num) {
+          return false;
+        }
+      }
+    }
+
+    return board;
+  }
+
+  solveSudoku(board, n) {
+    let row = -1;
+    let col = -1;
+    let isEmpty = true;
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < n; j++) {
+        if (board[i][j] == 0) {
+          row = i;
+          col = j;
+          isEmpty = false;
+          break;
+        }
+      }
+      if (!isEmpty) {
+        break;
+      }
+    }
+
+    if (isEmpty) {
+      return board;
+    }
+
+    for (let num = 1; num <= n; num++) {
+      if (this.isSafe(board, row, col, num)) {
+        board[row][col] = num;
+        if (this.solveSudoku(board, n)) {
+          return board;
+        } else {
+          board[row][col] = 0;
+        }
+      }
+    }
+    return false;
+  }
+
+  transform(puzzleString) {
+    let grid = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      ],
+      row = -1,
+      col = 0;
+    for (let i = 0; i < puzzleString.length; i++) {
+      if (i % 9 == 0) row++;
+      if (col % 9 == 0) col = 0;
+      grid[row][col] = puzzleString[i] === "." ? 0 : +puzzleString[i];
+      col++;
+    }
+    return grid;
+  }
+
+  transformBack(grid) {
+    return grid.flat().join("");
   }
 
   solve(puzzleString) {
-    
+    if(puzzleString.length!=81) return false;
+    if(/[^0-9.]/g.test(puzzleString)) return false;
+    let grid = this.transform(puzzleString);
+    let solved = this.solveSudoku(grid, N);
+    if (!solved) return false;
+    let solvedString = this.transformBack(solved);
+    return solvedString;
   }
 }
-
+//769235418851496372432178956174569283395842761628713549283657194516924837947381625
 module.exports = SudokuSolver;
-
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,11 +1,241 @@
 const chai = require("chai");
-const chaiHttp = require('chai-http');
+const chaiHttp = require("chai-http");
 const assert = chai.assert;
-const server = require('../server');
+const server = require("../server");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', () => {
+let validPuzzle =
+    "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..",
+  complete =
+    "769235418851496372432178956174569283395842761628713549283657194516924837947381625";
 
+suite("Functional Tests", () => {
+  test("Solve a puzzle with valid puzzle string: POST request to /api/solve", (done) => {
+    chai
+      .request(server)
+      .post("/api/solve")
+      .send({ puzzle: validPuzzle })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.body.solution, complete);
+        done();
+      });
+  });
+  test("Solve a puzzle with missing puzzle string: POST request to /api/solve", (done) => {
+    chai
+      .request(server)
+      .post("/api/solve")
+      .send({})
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.body.error, "Required field missing");
+        done();
+      });
+  });
+  test("Solve a puzzle with invalid characters: POST request to /api/solve", (done) => {
+    chai
+      .request(server)
+      .post("/api/solve")
+      .send({
+        puzzle:
+          "..9..5.1.85.4....2432..k...1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..",
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.body.error, "Invalid characters in puzzle");
+        done();
+      });
+  });
+  test("Solve a puzzle with incorrect length: POST request to /api/solve", (done) => {
+    chai
+      .request(server)
+      .post("/api/solve")
+      .send({
+        puzzle:
+          "..9..5.1.85.4....2432.....1...69.83.9.....6.62.71...9......1945....4.37.4.3..6.....",
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(
+          res.body.error,
+          "Expected puzzle to be 81 characters long"
+        );
+        done();
+      });
+  });
+  test("Solve a puzzle that cannot be solved: POST request to /api/solve", (done) => {
+    chai
+      .request(server)
+      .post("/api/solve")
+      .send({
+        puzzle:
+          "1.9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..",
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.body.error, "Puzzle cannot be solved");
+        done();
+      });
+  });
+  test("Check a puzzle placement with all fields: POST request to /api/check", (done) => {
+    const coordinate = "A1";
+    const value = "7";
+    const status = { valid: true };
+
+    chai
+      .request(server)
+      .post("/api/check")
+      .send({ puzzle: validPuzzle, coordinate, value })
+      .end((err, res) => {
+        assert.isObject(res.body);
+        assert.property(res.body, "valid");
+        assert.deepEqual(res.body, status);
+        done();
+      });
+  });
+  test("Check a puzzle placement with single placement conflict: POST request to /api/check", (done) => {
+    const coordinate = "A2";
+    const value = "1";
+    const status = { valid: false, conflict: ["row"] };
+
+    chai
+      .request(server)
+      .post("/api/check")
+      .send({ puzzle: validPuzzle, coordinate, value })
+      .end((err, res) => {
+        assert.isObject(res.body);
+        assert.property(res.body, "valid");
+        assert.property(res.body, "conflict");
+        assert.deepEqual(res.body, status);
+        done();
+      });
+  });
+  test("Check a puzzle placement with multiple placement conflicts: POST request to /api/check", (done) => {
+    const coordinate = "A1";
+    const value = "1";
+    const status = { valid: false, conflict: ["row", "column"] };
+
+    chai
+      .request(server)
+      .post("/api/check")
+      .send({ puzzle: validPuzzle, coordinate, value })
+      .end((err, res) => {
+        assert.isObject(res.body);
+        assert.property(res.body, "valid");
+        assert.property(res.body, "conflict");
+        assert.deepEqual(res.body, status);
+        done();
+      });
+  });
+  test("Check a puzzle placement with all placement conflicts: POST request to /api/check", (done) => {
+    const input =
+      "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..";
+    const coordinate = "A1";
+    const value = "5";
+    const status = { valid: false, conflict: ["row", "column", "region"] };
+
+    chai
+      .request(server)
+      .post("/api/check")
+      .send({ puzzle: input, coordinate, value })
+      .end((err, res) => {
+        assert.isObject(res.body);
+        assert.property(res.body, "valid");
+        assert.property(res.body, "conflict");
+        assert.deepEqual(res.body, status);
+        done();
+      });
+  });
+  test("Check a puzzle placement with missing required fields: POST request to /api/check", (done) => {
+    const input =
+      "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..";
+    const error = { error: "Required field(s) missing" };
+
+    chai
+      .request(server)
+      .post("/api/check")
+      .send({ puzzle: input })
+      .end((err, res) => {
+        assert.isObject(res.body);
+        assert.property(res.body, "error");
+        assert.deepEqual(res.body, error);
+        done();
+      });
+  });
+  test("Check a puzzle placement with invalid characters: POST request to /api/check", (done) => {
+    chai
+      .request(server)
+      .post("/api/solve")
+      .send({
+        puzzle:
+          "..9..5.1.85.4....2432..k...1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..",
+        coordinate: "A1",
+        value: "7",
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.body.error, "Invalid characters in puzzle");
+        done();
+      });
+  });
+  test("Check a puzzle placement with incorrect length: POST request to /api/check", (done) => {
+    chai
+      .request(server)
+      .post("/api/solve")
+      .send({
+        puzzle:
+          "..9..5.1.85.4....2432.....1...69.83.9.....6.62.71...9......1945....4.37.4.3..6.....",
+        coordinate: "A1",
+        value: "7",
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(
+          res.body.error,
+          "Expected puzzle to be 81 characters long"
+        );
+        done();
+      });
+  });
+  test("Check a puzzle placement with invalid placement coordinate: POST request to /api/check", (done) => {
+    const coordinate1 = "K1";
+    const coordinate2 = "A11";
+    const error = { error: "Invalid coordinate" };
+
+    chai
+      .request(server)
+      .post("/api/check")
+      .send({ puzzle: validPuzzle, coordinate: coordinate1, value: "2" })
+      .end((err, res) => {
+        assert.isObject(res.body);
+        assert.property(res.body, "error");
+        assert.deepEqual(res.body, error);
+
+        chai
+          .request(server)
+          .post("/api/check")
+          .send({ puzzle: validPuzzle, coordinate: coordinate2, value: "2" })
+          .end((errr, result) => {
+            assert.isObject(result.body);
+            assert.property(result.body, "error");
+            assert.deepEqual(result.body, error);
+            done();
+          });
+      });
+  });
+  test("Check a puzzle placement with invalid placement value: POST request to /api/check", (done) => {
+    const error = { error: "Invalid value" };
+
+    chai
+      .request(server)
+      .post("/api/check")
+      .send({ puzzle: validPuzzle, coordinate: "A1", value: "X" })
+      .end((err, res) => {
+        assert.isObject(res.body);
+        assert.property(res.body, "error");
+        assert.deepEqual(res.body, error);
+        done();
+      });
+  });
 });
-
